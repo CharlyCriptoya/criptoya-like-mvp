6e447e003213ad2dbf46d55e2396da5702b0ed95
CharlyCriptoya <charlybreakers24@gmail.com>
2025-09-07 13:02:25 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+// servir carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
+// fallback a index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
